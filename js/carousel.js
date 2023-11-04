872237e2c1143d325d39b32e0e0adc7937dd8e99
santiagoparedesn <125600949+santiagoparedesn@users.noreply.github.com>
2023-11-03 19:58:24 -0500

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,12 @@
+// Declaramos constante que contiene los elementos del carrusel
 const carousel = document.querySelector(".carouselItems");
 
-let maxScrollLeft = carousel.scrollWidth -carousel.clientWidth;
-
+// Declaramos variables para funcionamiento de carrusel 
+let maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
 let interval = null;
 let step = 1;
 
+// Funcion Start que contiene un intervalo de inicio de movimiento del carrusel
 const start = () => {
     interval = setInterval(function(){
         carousel.scrollLeft = carousel.scrollLeft + step;
@@ -16,16 +18,20 @@ const start = () => {
     }, 10);
 };
 
+// Funcion Stop que detiene el intervalo, esto detiene el carrusel de imagenes
 const stop = () => {
     clearInterval(interval);
 };
 
+// Añadimos evento que al colocar el mouse sobre el carrusel, se detiene.
 carousel.addEventListener('mouseover', () => {
     stop();
 })
 
+// Añadimos evento que al retirar el mouse del carrusel, se activa nuevamente.
 carousel.addEventListener('mouseout', () => {
     start();
 })
 
+// Al inicial la pagina llamamos la función de inicio de carrusel.
 start();
